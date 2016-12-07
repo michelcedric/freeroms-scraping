68bf50b21471a1eae1e7f73065effffa0a4f9b28
Fabian Vilers <fabian.vilers@dev-one.com>
2016-12-07 17:17:37 +0100

    Add async main

--- src/FreeromsScraping/Program.cs
+++ src/FreeromsScraping/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace FreeromsScraping
+﻿using Nito.AsyncEx;
+using System;
+using System.Threading.Tasks;
+
+namespace FreeromsScraping
 {
     internal class Program
     {
+        private static Task MainAsync()
+        {
+            throw new NotImplementedException();
+        }
+
         private static void Main()
         {
+            AsyncContext.Run(async () => await MainAsync().ConfigureAwait(false));
         }
     }
 }
